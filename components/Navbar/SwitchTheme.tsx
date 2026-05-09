5a5ca5018c9b2cce81252fea45b486db476c7447
dealexis <flytzitv@gmail.com>
2026-05-09 14:10:26 +0200

    feat: Refactor SwitchTheme component for improved theme handling and mounting logic

--- components/Navbar/SwitchTheme.tsx
+++ components/Navbar/SwitchTheme.tsx
@@ -1,20 +1,36 @@
 "use client";
+
+import { useState, useEffect } from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 export default function SwitchTheme() {
-  const { setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const handleToggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    setTheme(theme === "light" ? "dark" : "light");
   };
+
+  if (!mounted) {
+    return (
+      <div className="h-full aspect-square bg-button-theme rounded-full flex items-center justify-center">
+        <div className="h-4 w-4" />
+      </div>
+    );
+  }
 
   return (
     <button
       className="h-full aspect-square bg-button-theme hover:bg-button-theme-hover text-button-theme-text hover:text-button-theme-hover-text rounded-full flex flex-row items-center justify-center transition-all duration-500 cursor-pointer"
       onClick={handleToggleTheme}
+      aria-label="Toggle Theme"
     >
-      {useTheme().theme === "light" ? <Sun size={16} /> : <Moon size={16} />}
+      {theme === "light" ? <Sun size={16} /> : <Moon size={16} />}
     </button>
   );
 }
