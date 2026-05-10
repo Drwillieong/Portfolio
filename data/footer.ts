ad0ad25e9822678e37f6fd1a67473f07eb902ec2
dealexis <flytzitv@gmail.com>
2026-05-10 19:02:24 +0200

    feat: Add Footer component with dynamic links and styling

---
+++ data/footer.ts
@@ -0,0 +1,26 @@
+export const footerLinks = {
+  General: [
+    { id: 1, name: "Accueil", link: "/" },
+    { id: 2, name: "Bts", link: "/bts" },
+    { id: 3, name: "Ecole & Entreprise", link: "/works" },
+    { id: 4, name: "Ecosystem", link: "/ecosystem" },
+    { id: 5, name: "Contact", link: "/contact" },
+  ],
+  "A Props": [
+    { id: 1, name: "À propos", link: "/about" },
+    { id: 2, name: "Projets E5", link: "/e5" },
+    { id: 3, name: "Projets E6", link: "/e6" },
+    { id: 4, name: "Certifications", link: "/certif" },
+    { id: 5, name: "Veille", link: "/veilles" },
+  ],
+  Projets: [
+    { id: 1, name: "Projet 1", link: "/" },
+    { id: 2, name: "Projet 2", link: "/" },
+    { id: 3, name: "Projet 3", link: "/" },
+    { id: 4, name: "Projet 4", link: "/" },
+  ],
+  Légal: [
+    { id: 1, name: "Privacy Policy", link: "/privacy" },
+    { id: 2, name: "Terms & Conditions", link: "/terms" },
+  ],
+};
