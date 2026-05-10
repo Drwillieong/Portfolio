e0f8d51a9ef588fdac443acc967080275212866d
dealexis <flytzitv@gmail.com>
2026-05-10 19:02:47 +0200

    feat: Remove absolute positioning from Footer component

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import FooterCard from "../Footer/FooterCard";
 
 export default function Footer() {
   return (
-    <div className="w-full flex flex-col gap-0 p-10 absolute bottom-0 left-0 bg-background text-foreground">
+    <div className="w-full flex flex-col gap-0 p-10 bg-background text-foreground">
       <FooterCard />
 
       <hr className="w-full border border-border h-px mt-12 mb-10" />
