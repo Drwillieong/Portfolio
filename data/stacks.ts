cc5ebdc22e3b8bdba9c85394833d3f442e9488cf
FlytziTv <flytzitv@gmail.com>
2026-05-11 16:01:29 +0200

    feat: Add a file with all data of skill

---
+++ data/stacks.ts
@@ -0,0 +1,186 @@
+interface StackProps {
+  id: number;
+  name: string;
+  image: string;
+  categorie: string;
+  link: string;
+}
+
+export const Languages: StackProps[] = [
+  // Langages
+  {
+    id: 1,
+    name: "HTML",
+    image: "/images/skills/html.svg",
+    categorie: "Langages",
+    link: "https://developer.mozilla.org/docs/HTML",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    image: "/images/skills/css.svg",
+    categorie: "Langages",
+    link: "https://developer.mozilla.org/Web/CSS",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    image: "/images/skills/javascript.svg",
+    categorie: "Langages",
+    link: "https://developer.mozilla.org/docs/JavaScript",
+  },
+  {
+    id: 4,
+    name: "TypeScript",
+    image: "/images/skills/typescript.svg",
+    categorie: "Langages",
+    link: "https://www.typescriptlang.org",
+  },
+  {
+    id: 5,
+    name: "PHP",
+    image: "/images/skills/php.svg",
+    categorie: "Langages",
+    link: "https://www.php.net",
+  },
+  {
+    id: 6,
+    name: "Lua",
+    image: "/images/skills/lua.svg",
+    categorie: "Langages",
+    link: "https://www.lua.org",
+  },
+];
+
+export const Frameworks: StackProps[] = [
+  // Frameworks & Tech
+  {
+    id: 1,
+    name: "React",
+    image: "/images/skills/react.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://www.reactjs.org",
+  },
+  {
+    id: 2,
+    name: "Vue.js",
+    image: "/images/skills/vue.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://www.vuejs.org",
+  },
+  {
+    id: 3,
+    name: "Next.js",
+    image: "/images/skills/nextjs.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://www.nextjs.org",
+  },
+  {
+    id: 4,
+    name: "Tailwind CSS",
+    image: "/images/skills/tailwindcss.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://www.tailwindcss.com",
+  },
+  {
+    id: 5,
+    name: "Node.js",
+    image: "/images/skills/nodejs.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://www.nodejs.org",
+  },
+  {
+    id: 6,
+    name: "MySQL",
+    image: "/images/skills/mysql.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://www.mysql.com",
+  },
+  {
+    id: 7,
+    name: "Git",
+    image: "/images/logiciel/git.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://www.git-scm.com",
+  },
+  {
+    id: 8,
+    name: "Symfony",
+    image: "/images/skills/symfony.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://symfony.com",
+  },
+  {
+    id: 9,
+    name: "Apache",
+    image: "/images/skills/apache.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://www.apache.org",
+  },
+  {
+    id: 10,
+    name: "Débian",
+    image: "/images/skills/debian.svg",
+    categorie: "Frameworks & Tech",
+    link: "https://www.debian.org",
+  },
+];
+
+export const Logiciels: StackProps[] = [
+  {
+    id: 1,
+    name: "Visual Studio Code",
+    image: "/images/logiciel/vscode.svg",
+    categorie: "Éditeur de code",
+    link: "https://code.visualstudio.com/",
+  },
+  {
+    id: 2,
+    name: "Figma",
+    image: "/images/logiciel/figma.svg",
+    categorie: "Outil de design (UI/UX)",
+    link: "https://www.figma.com/fr-fr/",
+  },
+  {
+    id: 3,
+    name: "SolidWorks",
+    image: "/images/logiciel/solidworks.svg",
+    categorie: "Conception assistée par ordinateur (CAO)",
+    link: "https://www.solidworks.com/fr",
+  },
+  {
+    id: 4,
+    name: "AutoCad",
+    image: "/images/logiciel/autocad.svg",
+    categorie: "Conception assistée par ordinateur (CAO)",
+    link: "https://www.autodesk.com/fr/products/autocad/overview",
+  },
+  {
+    id: 5,
+    name: "Fusion 360",
+    image: "/images/logiciel/fusion360.svg",
+    categorie: "Conception assistée par ordinateur (CAO)",
+    link: "https://www.autodesk.com/fr/products/fusion-360/overview",
+  },
+  {
+    id: 6,
+    name: "Blender",
+    image: "/images/logiciel/blender.svg",
+    categorie: "Modélisation 3D / Animation",
+    link: "https://studio.blender.org/welcome/",
+  },
+  {
+    id: 7,
+    name: "Git",
+    image: "/images/logiciel/git.svg",
+    categorie: "Contrôle de version",
+    link: "https://git-scm.com/",
+  },
+  {
+    id: 8,
+    name: "Git Extension",
+    image: "/images/logiciel/gitextension.svg",
+    categorie: "Client Git (interface graphique)",
+    link: "https://gitextensions.github.io/",
+  },
+];
