456c7e6649c05e6f0474ac352c37a390bb46d7fc
dealexis <flytzitv@gmail.com>
2026-05-11 23:35:06 +0200

    feat: Add the About component to display information about the developer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/Section/About";
 import GithubGraph from "@/components/Section/GithubGraph";
 import Hero from "@/components/Section/Hero";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="max-w-212 mx-auto py-12 sm:py-24 px-6 space-y-10">
       <Hero />
+      <About />
       <GithubGraph />
     </main>
   );
---
+++ components/Section/About.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link";
+
+function About() {
+  return (
+    <section className="flex justify-start items-start gap-1.5 flex-col">
+      <h3 className="text-2xl font-bold">À Propos</h3>
+      <p className="text-(--color-text) text-sm">
+        Je suis
+        <span className="text-white font-medium mx-1">
+          développeur Full-Stack
+        </span>
+        passionné par la création d’applications web modernes, rapides et
+        responsives. Je travaille principalement avec <TextLink text="React" />,{" "}
+        <TextLink text="Tailwind CSS" />, <TextLink text="TypeScript" />
+        , <TextLink text="Next.js" />, <TextLink text="Vite" />,{" "}
+        <TextLink text="Express.js" /> et les technologies modernes du web. Je
+        suis toujours en train d’apprendre et de créer de nouveaux projets.
+      </p>
+    </section>
+  );
+}
+
+function TextLink({ text }: { text: string }) {
+  return (
+    <Link
+      href="#stack"
+      className="text-white font-medium underline-offset-2 underline"
+    >
+      {text}
+    </Link>
+  );
+}
+
+export default About;
