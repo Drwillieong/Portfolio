3916ad31bad4b51014267a5876a188e6c23c0821
FlytziTv <flytzitv@gmail.com>
2026-05-12 12:18:56 +0200

    feat: update stack categories for improved organization and clarity

--- data/stacks.ts
+++ data/stacks.ts
@@ -1,4 +1,4 @@
-interface StackProps {
+export interface StackProps {
   id: number;
   name: string;
   image: string;
@@ -7,180 +7,180 @@ interface StackProps {
 }
 
 export const Languages: StackProps[] = [
-  // Langages
+  // Frontend
   {
     id: 1,
     name: "HTML",
     image: "/images/skills/html.svg",
-    categorie: "Langages",
+    categorie: "Frontend",
     link: "https://developer.mozilla.org/docs/HTML",
   },
   {
     id: 2,
     name: "CSS",
     image: "/images/skills/css.svg",
-    categorie: "Langages",
+    categorie: "Frontend",
     link: "https://developer.mozilla.org/Web/CSS",
   },
   {
     id: 3,
     name: "JavaScript",
     image: "/images/skills/javascript.svg",
-    categorie: "Langages",
+    categorie: "Frontend",
     link: "https://developer.mozilla.org/docs/JavaScript",
   },
   {
     id: 4,
     name: "TypeScript",
     image: "/images/skills/typescript.svg",
-    categorie: "Langages",
+    categorie: "Frontend",
     link: "https://www.typescriptlang.org",
   },
+  // Backend & Scripting
   {
     id: 5,
     name: "PHP",
     image: "/images/skills/php.svg",
-    categorie: "Langages",
+    categorie: "Backend",
     link: "https://www.php.net",
   },
   {
     id: 6,
     name: "Lua",
     image: "/images/skills/lua.svg",
-    categorie: "Langages",
+    categorie: "Backend",
     link: "https://www.lua.org",
   },
 ];
 
 export const Frameworks: StackProps[] = [
-  // Frameworks & Tech
+  // Frontend
   {
     id: 1,
     name: "React",
     image: "/images/skills/react.svg",
-    categorie: "Frameworks & Tech",
+    categorie: "Frontend",
     link: "https://www.reactjs.org",
   },
   {
     id: 2,
     name: "Vue.js",
     image: "/images/skills/vue.svg",
-    categorie: "Frameworks & Tech",
+    categorie: "Frontend",
     link: "https://www.vuejs.org",
   },
   {
     id: 3,
     name: "Next.js",
     image: "/images/skills/nextjs.svg",
-    categorie: "Frameworks & Tech",
+    categorie: "Frontend",
     link: "https://www.nextjs.org",
   },
   {
     id: 4,
     name: "Tailwind CSS",
     image: "/images/skills/tailwindcss.svg",
-    categorie: "Frameworks & Tech",
+    categorie: "Frontend",
     link: "https://www.tailwindcss.com",
   },
+  // Backend
   {
     id: 5,
     name: "Node.js",
     image: "/images/skills/nodejs.svg",
-    categorie: "Frameworks & Tech",
+    categorie: "Backend",
     link: "https://www.nodejs.org",
   },
   {
     id: 6,
-    name: "MySQL",
-    image: "/images/skills/mysql.svg",
-    categorie: "Frameworks & Tech",
-    link: "https://www.mysql.com",
-  },
-  {
-    id: 7,
-    name: "Git",
-    image: "/images/logiciel/git.svg",
-    categorie: "Frameworks & Tech",
-    link: "https://www.git-scm.com",
-  },
-  {
-    id: 8,
     name: "Symfony",
     image: "/images/skills/symfony.svg",
-    categorie: "Frameworks & Tech",
+    categorie: "Backend",
     link: "https://symfony.com",
   },
+  // Base de données
   {
-    id: 9,
+    id: 7,
+    name: "MySQL",
+    image: "/images/skills/mysql.svg",
+    categorie: "Base de données",
+    link: "https://www.mysql.com",
+  },
+  // Serveur & Système
+  {
+    id: 8,
     name: "Apache",
     image: "/images/skills/apache.svg",
-    categorie: "Frameworks & Tech",
+    categorie: "Serveur & Système",
     link: "https://www.apache.org",
   },
   {
-    id: 10,
+    id: 9,
     name: "Débian",
     image: "/images/skills/debian.svg",
-    categorie: "Frameworks & Tech",
+    categorie: "Serveur & Système",
     link: "https://www.debian.org",
   },
 ];
 
 export const Logiciels: StackProps[] = [
+  // Développement & DevOps
   {
     id: 1,
     name: "Visual Studio Code",
     image: "/images/logiciel/vscode.svg",
-    categorie: "Éditeur de code",
+    categorie: "Développement",
     link: "https://code.visualstudio.com/",
   },
   {
     id: 2,
-    name: "Figma",
-    image: "/images/logiciel/figma.svg",
-    categorie: "Outil de design (UI/UX)",
-    link: "https://www.figma.com/fr-fr/",
-  },
-  {
-    id: 3,
-    name: "SolidWorks",
-    image: "/images/logiciel/solidworks.svg",
-    categorie: "Conception assistée par ordinateur (CAO)",
-    link: "https://www.solidworks.com/fr",
-  },
-  {
-    id: 4,
-    name: "AutoCad",
-    image: "/images/logiciel/autocad.svg",
-    categorie: "Conception assistée par ordinateur (CAO)",
-    link: "https://www.autodesk.com/fr/products/autocad/overview",
-  },
-  {
-    id: 5,
-    name: "Fusion 360",
-    image: "/images/logiciel/fusion360.svg",
-    categorie: "Conception assistée par ordinateur (CAO)",
-    link: "https://www.autodesk.com/fr/products/fusion-360/overview",
-  },
-  {
-    id: 6,
-    name: "Blender",
-    image: "/images/logiciel/blender.svg",
-    categorie: "Modélisation 3D / Animation",
-    link: "https://studio.blender.org/welcome/",
-  },
-  {
-    id: 7,
     name: "Git",
     image: "/images/logiciel/git.svg",
-    categorie: "Contrôle de version",
+    categorie: "DevOps & Versioning",
     link: "https://git-scm.com/",
   },
   {
-    id: 8,
+    id: 3,
     name: "Git Extension",
     image: "/images/logiciel/gitextension.svg",
-    categorie: "Client Git (interface graphique)",
+    categorie: "DevOps & Versioning",
     link: "https://gitextensions.github.io/",
+  },
+  // Design
+  {
+    id: 4,
+    name: "Figma",
+    image: "/images/logiciel/figma.svg",
+    categorie: "Design (UI/UX)",
+    link: "https://www.figma.com/fr-fr/",
+  },
+  // CAO & Modélisation
+  {
+    id: 5,
+    name: "SolidWorks",
+    image: "/images/logiciel/solidworks.svg",
+    categorie: "CAO & 3D",
+    link: "https://www.solidworks.com/fr",
+  },
+  {
+    id: 6,
+    name: "AutoCad",
+    image: "/images/logiciel/autocad.svg",
+    categorie: "CAO & 3D",
+    link: "https://www.autodesk.com/fr/products/autocad/overview",
+  },
+  {
+    id: 7,
+    name: "Fusion 360",
+    image: "/images/logiciel/fusion360.svg",
+    categorie: "CAO & 3D",
+    link: "https://www.autodesk.com/fr/products/fusion-360/overview",
+  },
+  {
+    id: 8,
+    name: "Blender",
+    image: "/images/logiciel/blender.svg",
+    categorie: "CAO & 3D",
+    link: "https://studio.blender.org/welcome/",
   },
 ];
