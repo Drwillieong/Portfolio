014661f1e2dc0b9e2f949e21c162805f8d0b1fc1
dealexis <flytzitv@gmail.com>
2026-05-12 21:23:13 +0200

    feat: Add share functionality with visual feedback in NavShare component

--- components/layout/navBar.tsx
+++ components/layout/navBar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Logo } from "../Icons/icon";
 import {
   Briefcase,
+  Check,
   HomeIcon,
   LucideIcon,
   Mail,
@@ -12,6 +13,7 @@ import {
 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { useState } from "react";
 
 export default function NavBar() {
   return (
@@ -80,13 +82,24 @@ function NavItem({
   );
 }
 
-// au click ca copie le lien de la page dans le presse-papier
-function NavShare() {
+// au click ca copie le lien de la page dans le presse-papier et on change dicon temporairement pour indiquer que le lien a été copié, puis on revient à l'icon de partage après 1.5s
+export function NavShare() {
   const pathname = usePathname();
+  const [isCopied, setIsCopied] = useState(false);
 
-  const handleShare = () => {
-    const url = `${window.location.origin}${pathname}`;
-    navigator.clipboard.writeText(url).then(() => {});
+  const handleShare = async () => {
+    try {
+      const url = `${window.location.origin}${pathname}`;
+      await navigator.clipboard.writeText(url);
+
+      setIsCopied(true);
+
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1500);
+    } catch (err) {
+      console.error("Erreur lors de la copie du lien : ", err);
+    }
   };
 
   return (
@@ -97,11 +110,23 @@ function NavShare() {
           data-slot="NavShare"
           className="flex aspect-square h-7 items-center justify-center text-muted-foreground hover:text-foreground transition-all duration-300 cursor-pointer active:scale-90"
         >
-          <Share size={16} />
+          <Share
+            size={16}
+            className={`absolute transition-all duration-300 ${
+              isCopied ? "scale-0 opacity-0" : "scale-100 opacity-100"
+            }`}
+          />
+
+          <Check
+            size={16}
+            className={`absolute text-green-500 transition-all duration-300 ${
+              isCopied ? "scale-100 opacity-100" : "scale-0 opacity-0"
+            }`}
+          />
         </button>
       </TooltipTrigger>
       <TooltipContent className="z-950 mb-4">
-        <p>Copier le lien</p>
+        <p>{isCopied ? "Lien copié !" : "Copier le lien"}</p>
       </TooltipContent>
     </Tooltip>
   );
