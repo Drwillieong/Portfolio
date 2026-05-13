b2c08847da0ede8ca435cddadc10e91ff0377233
dealexis <flytzitv@gmail.com>
2026-05-13 19:16:20 +0200

    feat: Add a certifications file with skills details

---
+++ data/certification.ts
@@ -0,0 +1,57 @@
+export const certifications = [
+  {
+    id: 1,
+    title: "Html",
+    issuer: "Codédex",
+    image: "/images/certif/codedex.gif",
+    pdf: "/files/certif/codédex_html.pdf",
+    description:
+      "Maîtrise de la structure sémantique, création de pages web accessibles, formulaires complets et bonnes pratiques de balisage.",
+    link: "https://www.codedex.io/html",
+    date: 2025,
+  },
+  {
+    id: 2,
+    title: "Css",
+    issuer: "Codédex",
+    image: "/images/certif/codedex.gif",
+    pdf: "/files/certif/codédex_css.pdf",
+    description:
+      "Compétences en mise en forme, layout responsif, gestion avancée des flexbox/grid et application des bonnes pratiques de design.",
+    link: "https://www.codedex.io/css",
+    date: 2025,
+  },
+  {
+    id: 3,
+    title: "Pix",
+    issuer: "Pix",
+    image: "/images/certif/pix.png",
+    pdf: "/files/certif/pix.pdf",
+    description:
+      "Validation des compétences numériques clés, sécurité informatique, gestion de l’information, résolution de problèmes et maîtrise des outils digitaux.",
+    link: "https://www.pix.fr",
+    date: 2024,
+  },
+  {
+    id: 4,
+    title: "SecNum Academie",
+    issuer: "ANSSI",
+    image: "/images/certif/secnum.png",
+    pdf: "/files/certif/secnum.pdf",
+    description:
+      "Connaissances en sécurité informatique, gestion des risques, protection des données et bonnes pratiques pour sécuriser les systèmes d'information.",
+    link: "https://secnumacademie.gouv.fr",
+    date: 2026,
+  },
+  {
+    id: 5,
+    title: "Responsive Web Design",
+    issuer: "freeCodeCamp",
+    image: "/images/certif/freecodecamp.png",
+    pdf: "https://www.freecodecamp.org/certification/flytzi/responsive-web-design-v9",
+    description:
+      "Compétences en design adaptatif, utilisation de media queries, flexbox et grid pour créer des interfaces web optimisées pour tous les appareils.",
+    link: "https://www.freecodecamp.org/learn/responsive-web-design-v9/",
+    date: 2025,
+  },
+];
