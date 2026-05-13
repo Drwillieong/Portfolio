8e5dd85289cb74c0fdcb9cd3263000e731c1d091
dealexis <flytzitv@gmail.com>
2026-05-13 18:37:19 +0200

    feat: Add About and Hero sections to the About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,11 @@
+import AboutProfile from "@/components/SectionAbout/About";
+import HeroAbout from "@/components/SectionAbout/Hero";
+
 export default function About() {
-  return <div></div>;
+  return (
+    <main className="max-w-212 mx-auto py-12 sm:py-24 px-6 space-y-10">
+      <HeroAbout />
+      <AboutProfile />
+    </main>
+  );
 }
