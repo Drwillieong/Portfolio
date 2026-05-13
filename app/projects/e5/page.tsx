8490128cb57713bb5584016492034d0aca354471
dealexis <flytzitv@gmail.com>
2026-05-13 16:06:22 +0200

    feat: Ajouter la gestion des projets E5, E6 et personnels avec les données correspondantes

--- app/projects/e5/page.tsx
+++ app/projects/e5/page.tsx
@@ -1,12 +1,19 @@
+import GridProject from "@/components/SectionProject/GridProject";
 import HeroCategoryProject from "@/components/SectionProject/HeroProject";
+import SwitchPages from "@/components/SectionProject/SwitchPages";
+import { projects_e5 } from "@/data/project-e5";
 
 export default function ProjectsE5() {
   return (
     <main className="max-w-212 mx-auto py-12 sm:py-24 px-6 space-y-10">
+      <SwitchPages />
+
       <HeroCategoryProject
         title="Projets E5"
         description="Découvrez mes projets réalisés dans le cadre de ma formation E5, mettant en avant mes compétences."
       />
+
+      <GridProject projects={projects_e5} />
     </main>
   );
 }
--- app/projects/perso/page.tsx
+++ app/projects/perso/page.tsx
@@ -1,12 +1,19 @@
+import GridProject from "@/components/SectionProject/GridProject";
 import HeroCategoryProject from "@/components/SectionProject/HeroProject";
+import SwitchPages from "@/components/SectionProject/SwitchPages";
+import { projects_perso } from "@/data/project-perso";
 
 export default function ProjectsPerso() {
   return (
     <main className="max-w-212 mx-auto py-12 sm:py-24 px-6 space-y-10">
+      <SwitchPages />
+
       <HeroCategoryProject
         title="Projets personnels"
         description="Découvrez mes projets personnels, réalisés en dehors de mes formations."
       />
+
+      <GridProject projects={projects_perso} />
     </main>
   );
 }
