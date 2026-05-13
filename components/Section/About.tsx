39b934fe50c44de956851f9047b7844c26571108
dealexis <flytzitv@gmail.com>
2026-05-13 18:37:52 +0200

    feat: Reorganize the text in the About section for clarity

--- components/Section/About.tsx
+++ components/Section/About.tsx
@@ -10,16 +10,17 @@ function About() {
     >
       <h3 className="text-2xl font-bold">À Propos</h3>
       <p className="text-foreground text-sm">
-        Je suis
-        <span className="text-foreground font-medium mx-1">
-          développeur Full-Stack
-        </span>
+        Je suis étudiant en{" "}
+        <span className="text-foreground font-medium">BTS SIO option SLAM</span>{" "}
+        et développeur
+        <span className="text-foreground font-medium mx-1">Full-Stack</span>
         passionné par la création d’applications web modernes, rapides et
         responsives. Je travaille principalement avec <TextLink text="React" />,{" "}
-        <TextLink text="Tailwind CSS" />, <TextLink text="TypeScript" />
-        , <TextLink text="Next.js" />, <TextLink text="Vite" />,{" "}
-        <TextLink text="Express.js" /> et les technologies modernes du web. Je
-        suis toujours en train d’apprendre et de créer de nouveaux projets.
+        <TextLink text="Tailwind CSS" />, <TextLink text="TypeScript" />,{" "}
+        <TextLink text="Next.js" />, <TextLink text="Vite" />,{" "}
+        <TextLink text="Express.js" /> et les technologies modernes du web.
+        J’aime apprendre continuellement, expérimenter de nouvelles technologies
+        et développer des projets concrets autour du web et du logiciel.
       </p>
     </section>
   );
