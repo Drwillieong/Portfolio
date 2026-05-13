b8e98676e0ef0fe74b9b5221c1d0e58b1b5382a0
dealexis <flytzitv@gmail.com>
2026-05-13 15:32:28 +0200

    feat: Add project categories and hero section components to enhance project display

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,11 @@
+import CategoryProject from "@/components/SectionProject/CategotyProject";
+import HeroCategoryProject from "@/components/SectionProject/HeroCategoryProject";
+
 export default function Projects() {
-  return <div></div>;
+  return (
+    <main className="max-w-212 mx-auto py-12 sm:py-24 px-6 space-y-10">
+      <HeroCategoryProject />
+      <CategoryProject />
+    </main>
+  );
 }
