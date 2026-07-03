f33b1934440b02e9860fce7eb18552869e7df8b7
dealexis <flytzitv@gmail.com>
2026-07-03 12:12:45 +0200

    chore: initialize project with Next.js, TypeScript, Tailwind CSS, and ESLint
    - Added package.json with dependencies and scripts for Next.js and Tailwind CSS.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added tsconfig.json for TypeScript configuration with strict settings.

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/layout/theme-provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Alexis De Jesus | Développeur Fullstack",
+  description:
+    "Portfolio d'Alexis De Jesus : Étudiant et Développeur Fullstack.",
+  icons: {
+    icon: "/images/sz-icons/icon.svg",
+    apple: "/images/sz-icons/icon.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="fr"
+      suppressHydrationWarning
+      className={`${geistSans.variable} h-full antialiased`}
+    >
+      <body className="bg-pattern">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return (
+    // <main className="max-w-212 mx-auto py-12 sm:py-24 px-6 space-y-10"></main>
+    <div></div>
+  );
+}
