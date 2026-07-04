82ea4c37245133e0dc2748d028081cac5e76a203
dealexis <flytzitv@gmail.com>
2026-07-04 15:19:47 +0200

    feat: ajouter le composant About à la page professionnelle et étudiante

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/section/all/about";
 import Hero from "@/components/section/all/Hero";
 
 export default function StudPage() {
   return (
-    <main className="max-w-250 mx-auto py-12 sm:py-24 px-6 space-y-10">
+    <main className="max-w-220 mx-auto py-12 sm:py-24 px-6 space-y-10">
       <Hero />
+      <About />
     </main>
   );
 }
