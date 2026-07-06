76fb4628438b428b8e471c037a05695bcb4b2043
dealexis <flytzitv@gmail.com>
2026-07-06 19:01:03 +0200

    feat: ajouter le composant Skills pour afficher les compétences

--- app/professional/page.tsx
+++ app/professional/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/section/all/about";
 import Hero from "@/components/section/all/Hero";
+import Skills from "@/components/section/all/skills";
 import ProjectPro from "@/components/section/professional/ProjectPro";
 import CompXp from "@/components/xp";
 import { workData } from "@/data/work";
@@ -11,6 +12,7 @@ export default function ProPage() {
       <About />
       <CompXp data={workData} id="work" title="Expérience professionnelle" />
       <ProjectPro />
+      <Skills />
     </main>
   );
 }
