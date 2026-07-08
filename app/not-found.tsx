b23aa3e72cc8e4c59cc6180f3a51025aa4e8cf76
dealexis <flytzitv@gmail.com>
2026-07-08 15:36:39 +0200

    feat: ajouter le composant NotFound pour gérer les erreurs 404

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <main className="mx-auto max-w-3xl px-4 py-8 sm:px-8 sm:py-12 min-h-screen flex flex-col justify-between">
+      <div className="flex flex-col gap-4 items-center justify-center grow text-center">
+        <h1 className="text-9xl font-bold tracking-tight">404</h1>
+        <p className="text-muted-foreground text-sm">
+          La page que vous recherchez n&apos;existe pas.
+        </p>
+        <Link href="/">
+          <Button
+            size="lg"
+            className="bg-foreground text-background hover:bg-foreground/85 px-4 cursor-pointer"
+          >
+            Retour à l&apos;accueil
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
+}
