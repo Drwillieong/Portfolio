30ad2826ca721bb17da27ae3bb9172254b580b2f
dealexis <flytzitv@gmail.com>
2026-07-08 15:10:38 +0200

    fix: ajuster l'espacement dans le composant Footer pour une meilleure présentation

--- components/section/all/footer.tsx
+++ components/section/all/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="relative mt-10 flex w-full flex-col items-center justify-center overflow-hidden border-t border-border/80 py-10">
+    <div className="relative flex w-full flex-col items-center justify-center overflow-hidden border-t border-border/80 pt-10">
       <p className="z-10 text-xs text-muted-foreground">
         © {new Date().getFullYear()} Mon Portfolio. Tous droits réservés.
       </p>
