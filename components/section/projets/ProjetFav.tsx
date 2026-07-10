f9b4850687095c7d6980ed3c80da69d0006c3e2e
dealexis <flytzitv@gmail.com>
2026-07-10 09:59:44 +0200

    feat: ajouter les composants HeroCategoryProject et CategoryProject pour afficher les projets favoris

---
+++ components/section/projets/ProjetFav.tsx
@@ -0,0 +1,65 @@
+import { MoveRight } from "lucide-react";
+
+import Link from "next/link";
+
+import { projects_e5 } from "@/data/projects/e5";
+import { projects_e6 } from "@/data/projects/e6";
+import { projects_perso } from "@/data/projects/perso";
+import ProjectCard from "@/components/card/CardProject";
+
+export default function CategoryProject() {
+  const categories = [
+    {
+      title: "Projets E5",
+      link: "/projects/e5",
+      data: projects_e5.filter((project) => project.favorite),
+    },
+    {
+      title: "Projets E6",
+      link: "/projects/e6",
+      data: projects_e6.filter((project) => project.favorite),
+    },
+    {
+      title: "Projets personnels",
+      link: "/projects/perso",
+      data: projects_perso.filter((project) => project.favorite),
+    },
+  ];
+
+  return (
+    <section
+      className="w-full relative flex flex-col gap-10"
+      id="projects-category"
+    >
+      {categories.map((category, index) => (
+        <div key={index} className="flex flex-col gap-2">
+          <Link
+            href={category.link}
+            className="flex flex-row items-center justify-start gap-4 group"
+          >
+            <h3 className="text-2xl font-bold">{category.title}</h3>
+
+            <p className="flex flex-row items-center gap-1 text-sm text-muted-foreground -translate-x-1.5 transition-all duration-500 ease-in-out group-hover:translate-x-0">
+              En voir plus
+              <MoveRight size={20} />
+            </p>
+          </Link>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            {category.data.map((project, index) => (
+              <ProjectCard
+                key={index}
+                imageSrc={project.image?.[0] || ""}
+                title={project.title}
+                description={project.desc}
+                role={project.role}
+                stacks={project.stack}
+                list={project.list}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+}
