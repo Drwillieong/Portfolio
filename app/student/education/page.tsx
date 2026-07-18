d5a4f9cee1f60c1d46b7637715da891ff8b0f69c
dealexis <flytzitv@gmail.com>
2026-07-18 21:56:58 +0200

    feat: ajouter le composant HeroEducation pour afficher les informations personnelles

--- app/student/education/page.tsx
+++ app/student/education/page.tsx
@@ -1,5 +1,9 @@
+import HeroEducation from "@/components/section/education/HeroEducation";
+
 export default function Education() {
   return (
-    <main className="max-w-220 mx-auto py-12 sm:py-24 px-6 space-y-10"></main>
+    <main className="max-w-220 mx-auto py-12 sm:py-24 px-6 space-y-10">
+      <HeroEducation />
+    </main>
   );
 }
